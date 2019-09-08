7412d0b8b70a949badcb13bec5c5d7997c8e34b5
andirsun <ander.laverde.dev@gmail.com>
2019-09-08 17:43:37 -0500

    upload some things

--- web-server/server.js
+++ web-server/server.js
@@ -5,18 +5,17 @@ const port = 3000;
 app.use(express.static(__dirname + '/public')); //asi me lee los archivos de la carpeta ppublc y cualquier persona podria acceder a http://localhost/3000/home.html
 
 //Express HBs
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');//handlebars una opcion por si no se quiere usar ninguno framework de frontends
 //////////////////////
-/*pp.get('/', (req, res) => { //de este modo solamente escucha las peticiones que son asi http://localhost/3000/
-    //res.send('Hello World!'); //despues de que pase por el / despues de la url 
-    let salida = {
-        nombre: 'Anderson',
-        edad: 19,
-        url: req.url
-    }
-    res.send(salida); // express en la funcion de send ya serializa si detecta un json
+app.get('/', (req, res) => { //de este modo solamente escucha las peticiones que son asi http://localhost/3000/
+    
 
-});*/
+    res.render('home',{
+        nombre:'Anderson', //asi cualquier variable que ponga en el html como {{nombre}} se remplaza por esta variable
+        anio: new Date().getFullYear()
+    }); //ais puedo renderizar vistas de los handlebars
+
+});
 app.get('/data', (req, res) => { //de este modo solamente escucha las peticiones que son asi http://localhost/3000/data
 
     res.send('Salida de cuando coloca slash data, aqui va la informaciond del data '); //despues de que pase por el / despues de la url 
